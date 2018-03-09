db1d90f3111d742e00e9056f4052fb0d69ace2bc
ryh <renyunhuixiaohui@gmail.com>
2018-03-09 15:03:56 +0800

    初始化express项目

--- app.js
+++ app.js
@@ -5,42 +5,44 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
-
+var routes = require('./routes/index');
+var settings = require('./settings');
+var flash = require('connect-flash');
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
+app.use(flash());
+app.use(session({
+    resave: true,  // 新增
+    saveUninitialized: true,  // 新增
+    secret: settings.cookieSecret,
+    key: settings.db,//cookie name
+    cookie: {maxAge: 1000 * 60 * 60 * 24 * 30},//30 days
+    store: new MongoStore({
+        db: settings.db,
+        host: settings.host,
+        port: settings.port
+    })
+}));
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(logger('dev')); //加载日志中间件
+app.use(bodyParser.json()); //加载解析json的中间件
+app.use(bodyParser.urlencoded({ extended: false })); //加载解析urlencoded请求体的中间件
+app.use(cookieParser()); //加载解析cookie的中间件
+app.use(express.static(path.join(__dirname, 'public'))); //设置public文件夹为存放静态文件的目录
 
-app.use('/', index);
-app.use('/users', users);
+/**
+ * 路由控制器
+ */
+routes(app);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.listen(app.get('port'), function() {
+    console.log('Express server listening on port ' + app.get('port'));
 });
 
 module.exports = app;
